121b7cfc41f73fd26d2d7896681b2cca7db80323
CreatureSurvive <cs@creaturecoding.com>
2017-12-01 17:51:43 -0600

    extra_recipe_dark
    Added Icon
    Added darker theme
    Added countdown timer
    Updated README
    Fixed layout

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-A jailbreak.
+![extra_recipe](repo_assets/extra_recipe.png)
+
+# extra_recipe jailbreak
 
 ##### Howto:
 
@@ -7,15 +9,20 @@ If it says "failed, reboot" you should really reboot.  Re-trying the same app wi
 
 ##### Support:
 
-| Device | Version |
-|---------|----------|
-| iPhone 7/+ | iOS 10.0.0 -> iOS 10.1.1 |
-| iPhone 6S/+ (TSMC) | iOS 10.2 |
-| iPad Air 2 | iOS 10.2 |
-| iPad Mini 3 (WiFi) | iOS 10.2 |
-| iPhone 6 | iOS 10.2 |
+| Device             | Version                  |
+| ------------------ | ------------------------ |
+| iPhone 7/+         | iOS 10.0.0 -> iOS 10.1.1 |
+| iPhone 6S/+ (TSMC) | iOS 10.2                 |
+| iPad Air 2         | iOS 10.2                 |
+| iPad Mini 3 (WiFi) | iOS 10.2                 |
+| iPhone 6           | iOS 10.2                 |
+
+##### Screenshots:
+
+![extra_recipe_screenshot](repo_assets/extra_recipe_screenshot.png)
 
 ##### Credits:
 
 * Ian Beer for the amazingly simple, yet awesome, kernel exploit
 * @qwertyoruiop for the amazingly complicated, yet effective, memprot bypass
+* @CreatureSurvive for not being blinded by the UI
--- extra_recipe/ViewController.swift
+++ extra_recipe/ViewController.swift
@@ -9,39 +9,68 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var bangButton: UIButton!
+    var seconds:Int = 30
+    var timer = Timer()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    //DispatchQueue.main.async(execute: { () -> Void in
-    //  jb_go();
-    //})
-  }
-
-
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
-
-  @IBAction func bang(_ sender: UIButton) {
-    var status: String
-    switch jb_go() {
-        case 0:
-            status = "jailbroken"
-        case 1:
-            status = "internal error"
-        case 2:
-            status = "unsupported"
-        case 3:
-            status = "unsupported yet"
-        case 42:
-            status = "hmm... ok"
-        default:
-            status = "failed, reboot"
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
-    sender.isEnabled = false
-    sender.setTitle(status, for: .disabled)
-  }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bangButton.isEnabled = false
+        bangButton.alpha = 0.6
+        runTimer()
+    }
+
+    func runTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self,   selector: (#selector(ViewController.updateTimer)),userInfo: nil, repeats: true)
+    }
+
+    func updateTimer() {
+        seconds -= 1
+        
+        if (seconds == 0) {
+            timer.invalidate()
+            bangButton.isEnabled = true
+            bangButton.alpha = 1
+            timerLabel.text = "ready!"
+        } else {
+            timerLabel.text = "stand by: \(seconds)s"
+        }
+    }
+    
+    func attempt() {
+        var status: String
+        
+        switch jb_go() {
+        case 0:
+            status = " jailbroken "
+        case 1:
+            status = " internal error "
+        case 2:
+            status = " unsupported "
+        case 3:
+            status = " unsupported yet "
+        case 42:
+            status = " hmm... ok "
+        default:
+            status = " failed, reboot "
+        }
+        
+        bangButton.setTitle(status, for: .disabled)
+    }
+    
+    @IBAction func bang(_ sender: UIButton) {
+        timerLabel.text = "fingers crossed"
+        sender.isEnabled = false
+        sender.alpha = 0.6
+        
+        attempt()
+    }
 
 }
 
